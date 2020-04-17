9be1b9a3cb4111085bfbd11ef663f6a6ae7859b8
Vinicius Menezes <viny2999@gmail.com>
2020-04-16 23:46:00 -0300

    create request to routes

--- src/app/graph-form/graph-form.component.ts
+++ src/app/graph-form/graph-form.component.ts
@@ -8,16 +8,36 @@ import { Graph } from "../models/graph";
   styleUrls: ['./graph-form.component.css']
 })
 export class GraphFormComponent {
+  public choice: string;
   public id: number;
   public source: string;
   public target: string;
+  public maxStops: string = '';
+
+  public showDistanceResponse = false;
+  public showRouteResponse = false;
+  public distance: number;
+  public paths: string[];
+  public routes: any[];
 
   constructor(private requestService: RequestService) {}
 
   async onSubmit() { 
-    let graph = new Graph(this.id, this.source.toUpperCase(), this.target.toUpperCase());
-    let res = await this.requestService.distanceRequest(graph);
-    console.log(res);
+    const graph = new Graph(this.id, this.source.toUpperCase(), this.target.toUpperCase(), this.maxStops);
+    let res;
+    if (this.choice == 'distance') {
+      res = await this.requestService.distanceRequest(graph);
+      this.distance = res.distance;
+      this.paths = res.path;
+      this.showDistanceResponse = true;
+    } else if (this.choice == 'route') {
+      res = await this.requestService.routeRequest(graph);
+      this.routes = res.routes;
+      this.showRouteResponse =  true;
+    }
+    
+    
+
   }
 
 }
--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -9,11 +9,18 @@ export class RequestService {
   private url = 'http://localhost:8080/';
   constructor(private http: HttpClient) { }
 
-  public async distanceRequest(graph: Graph) {
+  public async distanceRequest(graph: Graph): Promise<any> {
     return await this.request('distance', graph);
   }
 
+  public async routeRequest(graph: Graph): Promise<any> {
+    return await this.request('routes', graph);
+  }
+
   private async request(path, graph: Graph) {
-    return await this.http.post(`${this.url}${path}/${graph.id}/from/${graph.source}/to/${graph.target}`, {}).toPromise();
+    const response = graph.maxStops ? 
+      await this.http.post(`${this.url}${path}/${graph.id}/from/${graph.source}/to/${graph.target}?maxStops=${graph.maxStops}`, {}).toPromise() : 
+      await this.http.post(`${this.url}${path}/${graph.id}/from/${graph.source}/to/${graph.target}`, {}).toPromise();
+    return response;
   }
 }
